c05737a936e3083b4b81974d72de6c634439ae28
literale <43133944+literale@users.noreply.github.com>
2018-10-08 17:27:39 +0600

    Реализованны некоторые проверки
    Сделали проверку входа на пустые  и частично пустые данные

--- VKLogTestGlebova.py
+++ VKLogTestGlebova.py
@@ -10,13 +10,24 @@ driver.get("https://vk.com/")
 log_btn = driver.find_element_by_id('index_login_button')
 
 log_btn.click()
-# TODO убедиться, что мы не залогинились
+# TODO убедиться, что мы не залогинились. Сделано
+try:
+    log_btn = driver.find_element_by_id('index_login_button')
+except NoSuchElementException:
+    print("Мы залогинились!")
+
 
 input_em = driver.find_element_by_id('index_email')
 input_em.send_keys('Test@yandex.ru')
 
 log_btn.click()
-# TODO убедиться, что мы не залогинились
+# TODO убедиться, что мы не залогинились. Сделано
+
+try:
+    log_btn = driver.find_element_by_id('index_login_button')
+except NoSuchElementException:
+    print("Мы залогинились!")
+
 input_pass = driver.find_element_by_id('index_pass')
 input_pass.send_keys('123456T')
 
