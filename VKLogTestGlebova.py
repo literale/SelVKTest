472259858773dec5488dc29930ddd6ee1b71a0fa
literale <43133944+literale@users.noreply.github.com>
2018-10-08 17:20:22 +0600

    Добавила TODOшки
    Тудушки для более полного соответсвия сценария и скрипта (в будущем)

--- VKLogTestGlebova.py
+++ VKLogTestGlebova.py
@@ -10,20 +10,22 @@ driver.get("https://vk.com/")
 log_btn = driver.find_element_by_id('index_login_button')
 
 log_btn.click()
+# TODO убедиться, что мы не залогинились
 
 input_em = driver.find_element_by_id('index_email')
 input_em.send_keys('Test@yandex.ru')
 
 log_btn.click()
-
+# TODO убедиться, что мы не залогинились
 input_pass = driver.find_element_by_id('index_pass')
 input_pass.send_keys('123456T')
 
 log_btn.click()
-
+# TODO убедиться, мы на другой странице
 wait_for_element = WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.ID, "login_reg_button"))
     )
 
 log_btn = driver.find_element_by_id('login_reg_button')
 log_btn.click()
+# TODO убедиться, что мы вновь на первой странице
