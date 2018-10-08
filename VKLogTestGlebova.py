4f7d13831b2d5cae2a8b00e272e311c048ef8bed
literale <43133944+literale@users.noreply.github.com>
2018-10-08 17:53:45 +0600

    Исправленны комментарии

--- VKLogTestGlebova.py
+++ VKLogTestGlebova.py
@@ -5,24 +5,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox(executable_path=r"D:\\gecodrive\\geckodriver.exe")
+# заходим в вк
 driver.get("https://vk.com/")
-
+# Ищем и кликаем на кнопку логина
 log_btn = driver.find_element_by_id('index_login_button')
 
 log_btn.click()
-# TODO убедиться, что мы не залогинились. Сделано
+# Убеждаемся, что не залогинились (можно иначе)
 try:
     log_btn = driver.find_element_by_id('index_login_button')
     print("Мы  не залогинились!")
 except NoSuchElementException:
     print("Мы залогинились!")
 
-
+# Вводим логин
 input_em = driver.find_element_by_id('index_email')
 input_em.send_keys('Test@yandex.ru')
 
 log_btn.click()
-# TODO убедиться, что мы не залогинились. Сделано
+# Убеждаемся, что не залогинились (можно иначе)
 
 try:
     log_btn = driver.find_element_by_id('index_login_button')
@@ -30,12 +31,13 @@ try:
 except NoSuchElementException:
     print("Мы залогинились!")
 
+# вводим пароль
 input_pass = driver.find_element_by_id('index_pass')
 input_pass.send_keys('123456T')
 
 log_btn.click()
 
-# TODO убедиться, мы на другой странице
+# Убеждаемся что на другой странице
 wait_for_element = WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.ID, "login_reg_button"))
     )
@@ -44,10 +46,11 @@ if driver.current_url == "https://vk.com/":
 else:
     print("Мы сменили страницу!")
 
+# ищем кнопку регистрации и нажимаем
 log_btn = driver.find_element_by_id('login_reg_button')
 log_btn.click()
 
-# TODO убедиться, что мы вновь на первой странице
+# Убеждаемс, что мы вновь на первой странице
 wait_for_element = WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.ID, "ij_submit"))
     )
